113c927fbab91c35f6ade5454742fd27096d9c3c
fragoler <e.f.oleg@mail.ru>
2024-02-04 17:14:28 +0300

    save-3

---
+++ Content.Shared/Stories/Drip/DripComponent.cs
@@ -0,0 +1,19 @@
+using Content.Shared.Containers.ItemSlots;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Stories.Drip;
+
+/// <summary>
+///     Used for entities that can be opened, closed, and can hold one item. E.g., fire extinguisher cabinets.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class DripComponent : Component
+{
+    /// <summary>
+    ///     The <see cref="ItemSlot"/> that stores the actual item. The entity whitelist, sounds, and other
+    ///     behaviours are specified by this <see cref="ItemSlot"/> definition.
+    /// </summary>
+    [DataField, ViewVariables]
+    public ItemSlot DripPackedSlot = new();
+}
---
+++ Content.Shared/Stories/Drip/SharedDripSystem.cs
@@ -0,0 +1,47 @@
+using Content.Shared.Containers.ItemSlots;
+using Content.Shared.Interaction;
+using Content.Shared.Lock;
+using Content.Shared.Verbs;
+using Robust.Shared.Audio;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Containers;
+using Robust.Shared.Timing;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Stories.Drip;
+
+public abstract class SharedDripSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly ItemSlotsSystem _itemSlots = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<DripComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<DripComponent, ComponentRemove>(OnComponentRemove);
+        SubscribeLocalEvent<DripComponent, ComponentStartup>(OnComponentStartup);
+
+        SubscribeLocalEvent<DripComponent, EntInsertedIntoContainerMessage>(OnContainerModified);
+        SubscribeLocalEvent<DripComponent, EntRemovedFromContainerMessage>(OnContainerModified);
+    }
+
+    private void OnComponentInit(EntityUid uid, DripComponent drip, ComponentInit args)
+    {
+        _itemSlots.AddItemSlot(uid, "DripPacked", drip.DripPackedSlot);
+    }
+
+    private void OnComponentRemove(EntityUid uid, DripComponent drip, ComponentRemove args)
+    {
+        _itemSlots.RemoveItemSlot(uid, drip.DripPackedSlot);
+    }
+
+    private void OnComponentStartup(EntityUid uid, DripComponent drip, ComponentStartup args)
+    {
+
+    }
+
+    private void OnContainerModified(EntityUid uid, DripComponent cabinet, ContainerModifiedMessage args)
+    {
+
+    }
+}
